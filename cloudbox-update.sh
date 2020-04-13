2758bd46ece567091db37fac4c652b0f26499cee
Xan <git@xanban.xyz>
2020-04-13 21:05:39 +0800

    added cloudbox-update.sh

---
+++ cloudbox-update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ -z "$STY" ]; then exec screen -dm -S cloudbox-update /bin/bash "$0"; fi
+    cd $HOME/cloudbox || exit
+    git fetch
+    git reset --hard @{u}
+    sudo ansible-playbook cloudbox.yml --tags cloudbox,community,emby,bazarr
+    cd $HOME/community || exit
+    git fetch
+    git reset --hard @{u}
+    sudo ansible-playbook community.yml --tags jellyfin,alltube,speedtest,thelounge,sshwifty,kitana,deezloader-remix,ouroboros,vnstat,asshama,epms,airsonic,nextcloud
+    sudo ansible-playbook community.yml --tags jellyfin,alltube,speedtest,thelounge,sshwifty,kitana,deezloader-remix,ouroboros,vnstat,asshama,epms,airsonic,nextcloud
+    docker exec thelounge thelounge upgrade
+    sudo reboot

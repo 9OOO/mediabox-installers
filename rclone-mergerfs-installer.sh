f3a182c2f57a5f4beaec05a5cfc117c2803c6222
Xan <git@xanban.xyz>
2020-04-13 09:54:00 +0800

    add rclone-mergerfs installer

---
+++ rclone-mergerfs-installer.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Rclone VFS/MergerFS Installer/Updater Script by Xan#7777
+
+echo "Creating necessary folders..."
+    mkdir -p "$HOME"/Stuff
+    mkdir -p "$HOME"/Stuff/Local
+    mkdir -p "$HOME"/Stuff/Mount
+    mkdir -p "$HOME"/MergerFS
+    mkdir -p "$HOME"/.config/systemd/user
+
+echo "Stopping service files..."
+    systemctl --user disable --now mergerfs.service
+    systemctl --user disable --now rclone-vfs.service
+    systemctl --user disable --now rclone-normal.service
+
+echo "Killing all rclone/mergerfs instances..."
+    killall rclone
+    killall mergerfs
+
+echo "Removing service files and old binaries for upgrade..."
+    cd "$HOME"/.config/systemd/user
+    rm rclone*
+    rm mergerfs*
+    cd "$HOME"/bin
+    rm rclone
+    rm mergerfs
+    cd "$HOME"/scripts
+    rm rclone*
+
+echo "Installing rclone..."
+    mkdir -p "$HOME"/.rclone-tmp
+    cd "$HOME"/.rclone-tmp || exit
+    wget https://downloads.rclone.org/v1.50.2/rclone-v1.50.2-linux-amd64.zip -O "$HOME"/.rclone-tmp/rclone.zip
+    unzip rclone.zip
+    cp "$HOME"/.rclone-tmp/rclone-v*/rclone "$HOME"/bin
+    command -v rclone
+    rclone version
+echo ""
+sleep 2
+
+echo "Done. Installing mergerfs..."
+    mkdir -p "$HOME"/tmp
+    wget https://github.com/trapexit/mergerfs/releases/download/2.28.3/mergerfs_2.28.3.debian-stretch_amd64.deb -O "$HOME"/tmp/mergerfs.deb
+    dpkg -x "$HOME"/tmp/mergerfs.deb "$HOME"/tmp
+    mv "$HOME"/tmp/usr/bin/* "$HOME"/bin
+    command -v mergerfs
+    mergerfs -v
+echo ""
+sleep 2
+
+echo "Done. Downloading service files..."
+    sleep 2
+    cd "$HOME"/.config/systemd/user || exit
+    wget https://raw.githubusercontent.com/ultraseedbox/UltraSeedbox-Scripts/master/MergerFS-Rclone/Service%20Files/rclone-vfs.service
+    wget https://raw.githubusercontent.com/ultraseedbox/UltraSeedbox-Scripts/master/MergerFS-Rclone/Service%20Files/mergerfs.service
+    sed -i "s#/homexx/yyyyy#$HOME#g" "$HOME"/.config/systemd/user/rclone-vfs.service
+    sed -i "s#/homexx/yyyyy#$HOME#g" "$HOME"/.config/systemd/user/mergerfs.service
+
+# Upload Script Download
+    echo "Installing upload script..."
+    wget https://raw.githubusercontent.com/ultraseedbox/UltraSeedbox-Scripts/master/MergerFS-Rclone/Upload%20Scripts/rclone-uploader.service
+    wget https://raw.githubusercontent.com/ultraseedbox/UltraSeedbox-Scripts/master/MergerFS-Rclone/Upload%20Scripts/rclone-uploader.timer
+    sed -i "s#/homexx/yyyyy#$HOME#g" "$HOME"/.config/systemd/user/rclone-uploader.service
+    echo "Starting Upload script..."
+
+echo "Starting services..."
+    systemctl --user daemon-reload
+    systemctl --user enable --now rclone-vfs.service
+    systemctl --user enable --now mergerfs.service
+    systemctl --user enable --now rclone-uploader.service
+    systemctl --user enable --now rclone-uploader.timer
+
+echo "Script cleanup..."
+    rm -rf "$HOME"/.rclone-tmp
+        rm -rf "$HOME"/tmp
+
+echo ""
+sleep 5
+echo ""
+echo "Done."
+cd "$HOME" || exit
+exit

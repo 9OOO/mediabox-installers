881fc39c5d6f30f5109f177d75144cd3ddcf6874
Xan <git@xanban.xyz>
2020-04-19 17:39:42 +0800

    script cleanup

--- cloudbox-update.sh
+++ cloudbox-update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -z "$STY" ]; then exec screen -dm -S cloudbox-update /bin/bash "$0"; fi
-    cd $HOME/cloudbox || exit
+    cd "$HOME"/cloudbox || exit
     git fetch
     git reset --hard @{u}
     sudo ansible-playbook cloudbox.yml --tags cloudbox,community,emby,bazarr
-    cd $HOME/community || exit
+    cd "$HOME"/community || exit
     git fetch
     git reset --hard @{u}
     sudo ansible-playbook community.yml --tags jellyfin,speedtest,thelounge,sshwifty,kitana,deezloader-remix,vnstat,asshama,epms,nextcloud

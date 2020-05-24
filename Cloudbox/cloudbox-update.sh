41c653726e6707ba6a07afeb5f9c97cd83a062a0
Xan <git@xanban.xyz>
2020-05-25 01:14:36 +0800

    updated to latest revision

--- Cloudbox/cloudbox-update.sh
+++ Cloudbox/cloudbox-update.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
+
+# Personal Full Cloudbox Upgrade Script
+# I usually cron this every month
+
 if [ -z "$STY" ]; then exec screen -dm -S cloudbox-update /bin/bash "$0"; fi
     cd "$HOME"/cloudbox || exit
     git fetch
     git reset --hard @{u}
-    sudo ansible-playbook cloudbox.yml --tags cloudbox,community,emby,bazarr
+    sudo ansible-playbook cloudbox.yml --tags cloudbox,community,emby,bazarr,sabnzbd,plex_dupefinder
     cd "$HOME"/community || exit
     git fetch
+    git checkout develop
     git reset --hard @{u}
-    sudo ansible-playbook community.yml --tags jellyfin,speedtest,thelounge,sshwifty,kitana,deezloader-remix,vnstat,asshama,epms,nextcloud
-    sudo ansible-playbook community.yml --tags jellyfin,speedtest,thelounge,sshwifty,kitana,deezloader-remix,vnstat,asshama,epms,nextcloud
-    docker exec thelounge thelounge upgrade
+    sudo ansible-playbook community.yml --tags speedtest,sshwifty,kitana,vnstat,asshama,epms,nextcloud,requestrr,alltube
+    sudo ansible-playbook community.yml --tags speedtest,sshwifty,kitana,vnstat,asshama,epms,nextcloud,requestrr,alltube
+    sudo rm -rfv /opt/plex/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/Sub-Zero.bundle/
+    sudo rm -rfv /opt/plex/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/Trakttv.bundle/
+    sudo rm -rfv /opt/plex/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/WebTools.bundle/
+    sudo "$HOME"/scripts/iptables.sh
     sudo reboot

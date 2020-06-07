c799989eabb910586e18b226488db87e8764026f
Xan <git@xanban.xyz>
2020-06-07 20:00:26 +0800

    updated to latest revision

--- Cloudbox/cloudbox-update.sh
+++ Cloudbox/cloudbox-update.sh
@@ -5,17 +5,18 @@
 
 if [ -z "$STY" ]; then exec screen -dm -S cloudbox-update /bin/bash "$0"; fi
     cd "$HOME"/cloudbox || exit
+    sudo ansible-playbook cloudbox.yml --tags backup
     git fetch
     git reset --hard @{u}
-    sudo ansible-playbook cloudbox.yml --tags cloudbox,community,emby,bazarr,sabnzbd,plex_dupefinder
+    sudo ansible-playbook cloudbox.yml --tags cloudbox,community,emby,bazarr,plex_dupefinder
     cd "$HOME"/community || exit
     git fetch
     git checkout develop
     git reset --hard @{u}
-    sudo ansible-playbook community.yml --tags speedtest,sshwifty,kitana,vnstat,asshama,epms,nextcloud,requestrr,alltube
-    sudo ansible-playbook community.yml --tags speedtest,sshwifty,kitana,vnstat,asshama,epms,nextcloud,requestrr,alltube
+    sudo ansible-playbook community.yml --tags speedtest,sshwifty,kitana,vnstat,asshama,epms,nextcloud,requestrr
+    sudo ansible-playbook community.yml --tags speedtest,sshwifty,kitana,vnstat,asshama,epms,nextcloud,requestrr
     sudo rm -rfv /opt/plex/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/Sub-Zero.bundle/
     sudo rm -rfv /opt/plex/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/Trakttv.bundle/
     sudo rm -rfv /opt/plex/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/WebTools.bundle/
-    sudo "$HOME"/scripts/iptables.sh
+    sudo /home/xan/scripts/iptables.sh
     sudo reboot
